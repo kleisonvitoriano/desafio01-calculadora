217cc4a850490ed5bbc256d7282edb0c56b59f08
Kleison Vitoriano Da Silva <86889280+kleisonvitoriano@users.noreply.github.com>
2026-09-15 10:21:27 -0300

    Implement calculator interface

---
+++ src/components/Button/index.js
@@ -0,0 +1,9 @@
+const Button = () => {
+  return (
+    <div className="App">
+      <h1>ola</h1>
+    </div>
+  );
+}
+
+export default Button;
---
+++ src/components/Input/styles.js
@@ -0,0 +1,30 @@
+import styled from  'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 85px;
+    background-color: #1e05df;
+    
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto', sans-serif;
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #1e05df;
+        border: 0;
+        align: right;
+        padding: 0 10px;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+
+        color : #ffffff;
+
+        font-size: 24px;
+        font-family: 'Roboto', sans-serif;
+`
